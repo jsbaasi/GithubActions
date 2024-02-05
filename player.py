2e2be2c45bd947de2d33eac0ad6535f4a5561396
jsbaasi <jujhaar02@gmail.com>
2024-02-05 21:49:05 +0000

    Initial

---
+++ player.py
@@ -0,0 +1,28 @@
+class Player(object):
+
+    def __init__(self, initialAmount = 100):
+        if initialAmount == 0:
+            self.health = 0
+            self.alive = False
+        else:
+            self.health = initialAmount
+            self.alive = True
+
+    def hitPlayer(self, damageAmount):
+        if self.alive == False:
+            raise PlayerNotAlive("Player is not alive")
+        elif damageAmount >= self.health:
+            self.alive = False
+        else:
+            self.health -= damageAmount
+
+    def healPlayer(self, healAmount):
+        if self.alive == False:
+            raise PlayerNotAlive("Player is not alive")
+        elif (self.health + healAmount) >= 100:
+            self.health = 100
+        else:
+            self.health += healAmount
+
+class PlayerNotAlive(Exception):
+    pass
