ffb6c7a33d3d5edb09e4baa7ced9a229dc088b80
jsbaasi <jujhaar02@gmail.com>
2024-02-06 02:34:36 +0000

    dasd

--- player.py
+++ player.py
@@ -1,13 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/crash')
-def main():
-    print("Vulnerability")
-
-app.run(debug=True)
-
 class Player():
     """Class representing a player."""
 
